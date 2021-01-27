77fc8dea3a3df831b7c38496065ece314078ab7a
mmtakeuchi <mikemtaki@gmail.com>
2021-01-26 21:36:18 -0700

    commit tree 9b9924ad7d784836f0c72a1f6a81d2c5ba0d3da4

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    i = 1
+    multiples = []
+
+    while ( i < limit ) do
+        if (i % 3 == 0 || i % 5 == 0)
+            multiples.push(i)
+        end
+        i += 1
+    end
+
+    return multiples
+end
+
+def sum_multiples(limit)
+    nums = collect_multiples(limit)
+    return nums.reduce { |sum, n| sum + n }  
+end
